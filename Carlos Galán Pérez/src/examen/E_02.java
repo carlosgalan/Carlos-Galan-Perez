0fcc6cb9b1a13e5bfd83c76e09aa90dcbd180f13
carlosgalan <carlosgalanperez@gmail.com>
2014-12-15 16:39:26 +0100

    sf

---
+++ Carlos Galán Pérez/src/examen/E_02.java
@@ -0,0 +1,14 @@
+package examen;
+
+import java.util.Scanner;
+
+public class E_02 {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n;
+		System.out.println("Dime 10 numeros");
+
+	}
+
+}
